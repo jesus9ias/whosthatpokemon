3dbf073832ba4504d979198500972e329525b3a8
Jesus Escamilla <jesusescamilla@hotmail.com>
2022-11-27 19:18:47 -0600

    update styles

--- src/components/Pokemon.tsx
+++ src/components/Pokemon.tsx
@@ -1,24 +1,25 @@
 import axios from 'axios';
 import { signal, computed } from '@preact/signals';
+import './pokemon.css';
 
 const pokemonsData = signal([]);
 const correctOption = signal(0);
-const revealed = signal(true);
 const selectedCorrectOption = signal(false);
+const gameStarted = signal(false);
 const endedTurn = signal(false);
-const countTurns = signal(1);
+const countTurns = signal(0);
 const countSuccess = signal(0);
 
 const maxPokemons = 905;
 const maxOptions = 4;
 
-const hiddenImageStyle = computed(() => revealed.value ? '' : '-webkit-filter: brightness(0%);');
+const hiddenImageStyle = computed(() => countTurns.value === 0 || endedTurn.value ? '' : '-webkit-filter: brightness(0%);');
 
 const correctName = computed(() => pokemonsData.value.find((pokemon) => pokemon.id === correctOption.value).name);
 
 const pokemonImageUrl = computed(() => `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${correctOption.value}.png`);
 
-const canGoNext = computed(() => endedTurn.value);
+const startText = computed(() => gameStarted.value ? 'RESTART' : 'START');
 
 const getPokemonData = async (id: number) => {
   const response = await axios({
@@ -59,15 +60,16 @@ const getOptions = () => {
 }
 
 const reset = () => {
+  gameStarted.value = false;
   pokemonsData.value = [];
   correctOption.value = 0;
-  revealed.value = false;
   selectedCorrectOption.value = false;
   endedTurn.value = false;
 };
 
 const start = () => {
   reset();
+  gameStarted.value = true;
   countTurns.value = 1;
   countSuccess.value = 0;
   getAll(getOptions());
@@ -85,27 +87,32 @@ const review = (id: number) => {
     selectedCorrectOption.value = true;
     countSuccess.value++;
   }
-  revealed.value = true;
   endedTurn.value = true;
 }
 
 export default function Pokemon() {
 	return (
 		<>
-    <div class="pokemon">
+    <div class="pokemon text-center">
       <div
-        class="pokemon__image"
+        class="pokemon__image m-auto"
         style={`background-image: url("${pokemonImageUrl.value}");${hiddenImageStyle.value}`}
       >
       </div>
       <p>{countSuccess.value}/{countTurns.value}</p>
       {
         pokemonsData.value.map((pokemon) => (
-          <button disabled={endedTurn.value} onClick={() => review(pokemon.id)}>{pokemon.name}</button>
+          <button class="button button-gray" disabled={endedTurn.value} onClick={() => review(pokemon.id)}>{pokemon.name}</button>
         ))
       }
-      <button onClick={() => start()}>Start</button>
-      <button disabled={!canGoNext.value} onClick={() => next()}>Next</button>
+      <div>
+        {
+          endedTurn.value ? <button class="button button-dark" onClick={() => next()}>NEXT</button> : null
+        }
+      </div>
+      <div>
+        <button class="button button-dark" onClick={() => start()}>{startText}</button>
+      </div>
       {
         endedTurn.value ? <h2>{correctName.value}</h2> : null
       }
